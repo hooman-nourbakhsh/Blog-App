8e3b3808f84c417a0f7e61fa474331174d8e0a57
HooMaN <eng.homan@gmail.com>
2025-07-25 14:41:48 +0330

    feat: :sparkles: I implemented the dynamic category list
    I created a server component to fetch and display the list of blog categories dynamically.
    
    - __`CategoryList` Component__: Developed a new server component (`CategoryList.jsx`) that fetches category data from the API endpoint (`/api/category/list`).
    - __API Integration__: The component uses `fetch` to get the data and renders a list of links for each category, including an "All" link.
    - __Blogs Layout__: Integrated the `CategoryList` component into the sidebar of the blogs page layout, replacing the static placeholder.
    - __Environment Variable__: The API URL is sourced from `process.env.NEXT_PUBLIC_API_URL`, making the endpoint configurable.
    
    This change makes the category navigation dynamic and data-driven.

---
+++ src/app/blogs/_components/CategoryList.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default async function CategoryList() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/category/list`);
+  const {
+    data: { categories },
+  } = await res.json();
+  return (
+    <ul className="space-y-4">
+      <Link href="/blogs">همه</Link>
+      {categories.map((category) => (
+        <li key={category._id}>
+          <Link href={`/blogs/category/${category.slug}`}>{category.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
